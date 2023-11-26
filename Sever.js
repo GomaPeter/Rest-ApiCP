a6a77a5be68eab1d44c2175f72cdd956b02f4615
GomaPeter <gomapeter40@gmail.com>
2023-11-26 09:23:57 +0100

    RestAPI

---
+++ Sever.js
@@ -0,0 +1,67 @@
+// Import necessary modules
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+/// Connect to MongoDB using mongoose
+mongoose.connect(process.env.MONGO_URI);
+
+// Create User model
+const User = require('./models/User.js');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+
+// GET: Return all users
+app.get('/Users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// POST: Add a new user to the database
+app.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// PUT: Edit a user by ID
+app.put('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// DELETE: Remove a user by ID
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
